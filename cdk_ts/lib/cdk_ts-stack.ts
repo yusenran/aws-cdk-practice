44fb71a6cbddf3502bfef99b37dadc34387f828c
yusenran <wired.ran@gmail.com>
2022-11-29 11:14:52 +0900

    [add/change]add s3 and change vpc name

--- cdk_ts/lib/cdk_ts-stack.ts
+++ cdk_ts/lib/cdk_ts-stack.ts
@@ -1,12 +1,13 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class CdkTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, "VPC", {
+    const vpc = new ec2.Vpc(this, "CdkTsPracticeVPC", {
       cidr: "10.0.0.0/16",
       maxAzs: 3,
       natGateways: 0,
@@ -22,6 +23,10 @@ export class CdkTsStack extends cdk.Stack {
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
         },
       ],
+    });
+
+    const bucket = new s3.Bucket(this, "CdkTsPracticeBucket", {
+      bucketName: "cdk-ts-practice-bucket",
     });
   }
 }
